760608e95293fc1f94aa07cb18e9909ab9310518
mia2583 <mia2583@pusan.ac.kr>
2023-01-08 20:05:52 +0900

    [BOJ] 팔 / 실버1 / +13분(재시도) - 기존의 브루트포스 풀이가 시간 초과되어 다시 풂 - 공통된 숫자에서만 8의 개수 세기

--- baekjoon/eight.cpp
+++ baekjoon/eight.cpp
@@ -23,11 +23,27 @@ int countEight(int number) {
     return eightN;
 }
 
+// R과 L의 공통 숫자 시작 위치 구하기
+int findDiffLen() {
+    int diff = R-L;
+    int diffLen = 1;
+    while(diff) {
+        diffLen*=10;
+        diff/=10;
+    }
+    return diffLen;
+}
+
 void solve() {
-    for(int i=L; i<=R; i++) {
-        eightN = countEight(i);
-        // 가장 적은 8의 개수
-        if(eightN < answer) answer=eightN;
+    // R과 L이 동일하면 바로 8의 개수 구하기
+    if(R-L==0) answer = countEight(R);
+    // 두 숫자의 차이가 있으면
+    // 공통 숫자 외에는 반드시 8이 0개 들어가는 자연수가 있음.
+    // 따라서 공통 숫자에서만 8의 개수를 셀 것.
+    else {
+        int len = findDiffLen();
+        answer = countEight(R/len);
+        if(countEight(L/len)<answer) answer = countEight(L/len);
     }
 }
 
