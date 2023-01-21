2b34940fe03a6b77f4b699185a50603ee0e80063
mia2583 <mia2583@pusan.ac.kr>
2023-01-21 23:42:28 +0900

    [BOJ] 단어의 개수 / 브론즈2 / 28분 - cin으로 하면 띄어쓰기도 \n으로 인식하므로 getline으로 입력 받아야 함. - 각 문장의 앞뒤의 띄어쓰기가 있는 경우도 고려해야 함. - 일부 케이스 통과 못함.

---
+++ baekjoon/countOfWords.cpp
@@ -0,0 +1,38 @@
+// 1152번 단어의개수 브론즈2
+// https://www.acmicpc.net/problem/1152
+// 28분
+#include<bits/stdc++.h>
+using namespace std;
+
+string sentence;
+int answer = 0;
+
+void input() {
+    getline(cin, sentence);
+}
+
+void solve() {
+    int before=0;
+    int now=0;
+    for(char word: sentence) {
+        if(word==' ') {
+            if(before!=now) {
+                answer++;
+                before=now;
+            }
+        }
+        now++;
+    }
+    if(now-before>1) answer++;
+}
+
+void output() {
+    cout << answer << endl;
+}
+
+int main() {
+    input();
+    solve();
+    output();
+    return 0;
+}
